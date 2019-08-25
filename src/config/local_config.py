a546f415a73ff39ba67083f5eed9a932f68a3ae9
klaudia-firlag <klaudia.firlag@gmail.com>
2019-08-25 17:07:31 +0200

    Added printing twitter basic informations

---
+++ src/config/local_config.py
@@ -0,0 +1,5 @@
+cons_key = 'FsSqXzVSXDzfOJOIbkSQ75b3c'
+cons_sec = 'UYb2eArXVxHmwOK9kz2Wivp3pSzuweqGBXWKyTpGHURqjKtWq0'
+
+app_key = '1164630710734512128-BCbMdEzj9K6uq3tc8pCczZ7KUgFZn9'
+app_sec = 'D0ac5760vJdpobYVnlDFsFXX0dNmqQcfDSEFK7UUvkPZu'
---
+++ src/twitter_server.py
@@ -0,0 +1,37 @@
+import tweepy
+
+import src.config.data as data
+from src.config.local_config import *
+from src.config.style import color
+
+
+def print_tweets_with_query(query):
+    search_results = api.search(q=query)
+    print(f"{color.BLUE}Tweets that include keyword "
+          f"{color.ITALIC}{query}{color.END}:\n")
+    for i in range(len(search_results)):
+        print(f"{getattr(search_results[i], 'text')}\n"
+              + "_" * 100)
+
+
+def print_top_trends(region: str):
+    try:
+        trends = api.trends_place(data.woeid[region])
+    except KeyError:
+        print(f"Invalid region. Aviable regions:\n{list(data.woeid.keys())}")
+        return
+
+    print(f"{color.BLUE}Top global trends:{color.END}\n")
+    trends = trends[0]['trends']
+    for i in range(len(trends)):
+        print(f"#{i + 1}\t{trends[i]['name']}")
+
+
+if __name__ == "__main__":
+    auth = tweepy.OAuthHandler(cons_key, cons_sec)
+    auth.set_access_token(app_key, app_sec)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+
+    print_tweets_with_query("travel")
+    print_top_trends("Worldwide")
